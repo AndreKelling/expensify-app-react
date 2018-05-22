7c2b07adb00cd4af3beb048d7c087d01e1194377
André J. Kelling <kontakt@andrekelling.de>
2018-05-22 14:54:07 +0200

    Setup data fetching from firebase

--- src/app.js
+++ src/app.js
@@ -7,14 +7,10 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {addExpense} from "./actions/expenses";
+import { startSetExpenses } from "./actions/expenses";
 import './firebase/firebase';
 
 const store = configureStore();
-
-// store.dispatch(addExpense({description:'Water bill', amount: 100, createdAt: 1000}));
-// store.dispatch(addExpense({description:'Gas bill', amount: 333, createdAt: 15000}));
-// store.dispatch(addExpense({description:'Rent', amount: 109500, createdAt: 1000}));
 
 const jsx = (
     <Provider store={store}>
@@ -22,4 +18,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
